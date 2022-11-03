908586cf50a3d837c3c8d7f79ab24222eed777cd
Dinara L <dinaralatypova16@mail.ru>
2022-11-03 17:29:47 +0500

    task2: change2

--- module_2/task2/task2.cpp
+++ module_2/task2/task2.cpp
@@ -1,26 +1,21 @@
 #include <algorithm>
 #include <iostream>
+#include <tuple>
 #include <vector>
 
-struct data {
+struct date {
     int year = 0;
     int month = 0;
     int day = 0;
     bool alive = true;
 };
 
-bool Comparator(data data1, data data2) {
-    if (data1.year == data2.year) {
-        if (data1.month == data2.month) {
-            return data1.day < data2.day;
-        }
-        return data1.month < data2.month;
-    }
-    return data1.year < data2.year;
+bool Comparator(date &date1, date &date2) {
+    return std::tie(date1.year, date1.month, date1.day) < std::tie(date2.year, date2.month, date2.day);
 }
 
-bool change(data &birth, data &death) {  // изменение даты рождения и смерти на период, когда человек мог быть ровестником
-    data eighty = birth;
+bool change(date &birth, date &death) {  // изменение даты рождения и смерти на период, когда человек мог быть ровестником
+    date eighty = birth;
     eighty.year += 80;
     eighty.alive = false;
     if (!Comparator(death, eighty)) {
@@ -30,27 +25,25 @@ bool change(data &birth, data &death) {  // изменение даты рожд
     return Comparator(birth, death);  // если birth >= death, человек не мог быть ровестником ни для кого
 }
 
-void SiftDown(std::vector<data> &array, int Size, int i) {
+void SiftDown(std::vector<date> &array, int Size, int i) {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
     int largest = i;
-    if (left < Size && Comparator(array[i], array[left]))
-        largest = left;
-    if (right < Size && Comparator(array[largest], array[right]))
-        largest = right;
+    if (left < Size && Comparator(array[i], array[left])) largest = left;
+    if (right < Size && Comparator(array[largest], array[right])) largest = right;
     if (largest != i) {
         std::swap(array[i], array[largest]);
         SiftDown(array, Size, largest);
     }
 }
 
-void BuildHeap(std::vector<data> &array, int Size) {
+void BuildHeap(std::vector<date> &array, int Size) {
     for (int i = Size / 2 - 1; i >= 0; --i) {
         SiftDown(array, Size, i);
     }
 }
 
-void HeapSort(std::vector<data> &array) {
+void HeapSort(std::vector<date> &array) {
     int heapSize = array.size();
     BuildHeap(array, heapSize);
     for (; heapSize > 1; --heapSize) {
@@ -59,14 +52,15 @@ void HeapSort(std::vector<data> &array) {
     }
 }
 
-
-int peers(std::vector<data> &dates) {
+int peers(std::vector<date> &dates) {
     HeapSort(dates);
     int count = 0;
     int Max = 0;  // будущий ответ
     for (int i = 0; i < dates.size(); ++i) {
         count += dates[i].alive ? 1 : -1;
-        if (i < dates.size() && dates[i].year == dates[i + 1].year && dates[i].month == dates[i + 1].month && dates[i].day == dates[i + 1].day) {
+        if (i < dates.size() && dates[i].year == dates[i + 1].year &&
+            dates[i].month == dates[i + 1].month &&
+            dates[i].day == dates[i + 1].day) {
             continue;
         }
         Max = std::max(Max, count);
@@ -77,9 +71,9 @@ int peers(std::vector<data> &dates) {
 int main() {
     int n = 0;
     std::cin >> n;
-    std::vector<data> dates;
+    std::vector<date> dates;
     for (int i = 0; i < n; ++i) {
-        data read;
+        date read;
         std::cin >> read.day >> read.month >> read.year;
         dates.push_back(read);
         std::cin >> read.day >> read.month >> read.year;
