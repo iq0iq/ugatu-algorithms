f7bef3096271bb07e0dd8b046bbb6f45d14c40e0
Dinara L <dinaralatypova16@mail.ru>
2023-02-15 22:33:22 +0500

    task1: change1

--- term2/module_1/task1/task1.cpp
+++ term2/module_1/task1/task1.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <vector>
 
+unsigned int counting(std::vector<int> &x, std::vector<unsigned int> &dp,
+                      std::vector<unsigned int> &last_in) {
+  dp[0] = 0;
+  for (unsigned int i = 1; i < dp.size(); ++i) {
+    for (int j = 0; j < x.size(); ++j) {
+      if (i >= x[j]) {
+        if (dp[i - x[j]] + 1 < dp[i]) {
+          last_in[i] = x[j];
+          dp[i] = dp[i - x[j]] + 1;
+        }
+      }
+    }
+  }
+  if (dp[dp.size() - 1] == 4'294'967'294) {
+    return -1;
+  } else {
+    return dp[dp.size() - 1];
+  }
+}
+
 int main() {
   int n = 0;
   std::cin >> n;
@@ -12,22 +32,7 @@ int main() {
   std::cin >> s;
   std::vector<unsigned int> dp(s + 1, 4'294'967'294);
   std::vector<unsigned int> last_in(s + 1, 0);
-  dp[0] = 0;
-  for (unsigned int i = 1; i < s + 1; ++i) {
-    for (int j = 0; j < n; ++j) {
-      if (i >= x[j]) {
-        if (dp[i - x[j]] + 1 < dp[i]) {
-          last_in[i] = x[j];
-          dp[i] = dp[i - x[j]] + 1;
-        }
-      }
-    }
-  }
-  if (dp[s] == 4'294'967'294) {
-    std::cout << -1 << std::endl;
-  } else {
-    std::cout << dp[s] << std::endl;
-  }
+  std::cout << counting(x, dp, last_in) << std::endl;
   if (last_in[s] != 0) {
     while (last_in[s] != 0) {
       std::cout << last_in[s] << ' ';
