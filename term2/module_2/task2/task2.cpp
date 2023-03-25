1e4200b450e7e4417f5a4eb5ec3f0437f7ac104d
Dinara L <dinaralatypova16@mail.ru>
2023-03-25 21:38:05 +0500

    task2: change3

--- term2/module_2/task2/task2.cpp
+++ term2/module_2/task2/task2.cpp
@@ -3,19 +3,19 @@
 
 class Graph {
 public:
-  explicit Graph(int vertices_count);
+  explicit Graph(std::size_t vertices_count);
 
   void AddEdge(std::size_t from, std::size_t to);
 
   std::size_t VerticesCount() const;
 
-  std::vector<int> FindAllAdjacent(std::size_t vertex) const;
+  std::vector<std::size_t> FindAllAdjacent(std::size_t vertex) const;
 
 private:
-  std::vector<std::vector<int>> edges;
+  std::vector<std::vector<std::size_t>> edges;
 };
 
-Graph::Graph(int vertices_count) : edges(vertices_count) {}
+Graph::Graph(std::size_t vertices_count) : edges(vertices_count) {}
 
 void Graph::AddEdge(std::size_t from, std::size_t to) {
   edges[to].emplace_back(from);
@@ -24,7 +24,7 @@ void Graph::AddEdge(std::size_t from, std::size_t to) {
 
 std::size_t Graph::VerticesCount() const { return edges.size(); }
 
-std::vector<int> Graph::FindAllAdjacent(std::size_t vertex) const {
+std::vector<std::size_t> Graph::FindAllAdjacent(std::size_t vertex) const {
   return edges[vertex];
 }
 
@@ -36,38 +36,34 @@ bool DFS(Graph &graph, std::vector<bool> &used, std::vector<bool> &color,
       color[i] = !color[current];
       if (!DFS(graph, used, color, i))
         return false;
-    }
-    if (used[i] && color[current] == color[i])
+    } else if (color[current] == color[i])
       return false;
   }
   return true;
 }
 
 bool MainDFS(Graph &graph) {  // returns true if the graph is bipartite
-  std::size_t Size = graph.VerticesCount();
-  std::vector<bool> used(Size);
-  std::vector<bool> color(Size);  // false - first part of vertexes, true - second
-  for (int i = 0; i < Size; ++i)
+  std::size_t size = graph.VerticesCount();
+  std::vector<bool> used(size);
+  std::vector<bool> color(
+      size);  // false - first part of vertexes, true - second
+  for (int i = 0; i < size; ++i)
     if (!used[i] && !DFS(graph, used, color, i))
       return false;
   return true;
 }
 
 int main() {
-  int Size = 0;
+  int size = 0;
   int n = 0;
-  std::cin >> Size >> n;
-  int from = 0;
-  int to = 0;
-  Graph graph(Size);
+  std::cin >> size >> n;
+  std::size_t from = 0;
+  std::size_t to = 0;
+  Graph graph(size);
   for (int i = 0; i < n; ++i) {
     std::cin >> from >> to;
     graph.AddEdge(from, to);
   }
-  if (MainDFS(graph)) {
-    std::cout << "YES" << std::endl;
-  } else {
-    std::cout << "NO" << std::endl;
-  }
+  std::cout << (MainDFS(graph) ? "YES" : "NO") << std::endl;
   return 0;
 }
