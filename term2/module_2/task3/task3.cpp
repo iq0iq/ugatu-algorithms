e46ca6922d598be8242fc1c76c20d469bdb23e06
Dinara L <dinaralatypova16@mail.ru>
2023-03-31 13:55:39 +0500

    task3: change1

--- term2/module_2/task3/task3.cpp
+++ term2/module_2/task3/task3.cpp
@@ -1,52 +1,54 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 class ListGraph {
 public:
-  explicit ListGraph(int vertices_count);
+  explicit ListGraph(std::size_t vertices_count);
 
-  void AddEdge(int from, int to);
+  void AddEdge(std::size_t from, std::size_t to);
   std::size_t VerticesCount() const;
-  std::vector<int> FindAllAdjacentIn(int vertex) const;
-  std::vector<int> FindAllAdjacentOut(int vertex) const;
+  std::vector<std::size_t> FindAllAdjacentIn(std::size_t vertex) const;
+  std::vector<std::size_t> FindAllAdjacentOut(std::size_t vertex) const;
 
 private:
-  std::vector<std::vector<int>> in_edges_;
-  std::vector<std::vector<int>> out_edges_;
+  std::vector<std::vector<std::size_t>> in_edges_;
+  std::vector<std::vector<std::size_t>> out_edges_;
 };
 
-ListGraph::ListGraph(int vertices_count)
+ListGraph::ListGraph(std::size_t vertices_count)
     : in_edges_(vertices_count), out_edges_(vertices_count) {}
 
-void ListGraph::AddEdge(int from, int to) {
+void ListGraph::AddEdge(std::size_t from, std::size_t to) {
   in_edges_[to].emplace_back(from);
   out_edges_[from].emplace_back(to);
 }
 
 std::size_t ListGraph::VerticesCount() const { return in_edges_.size(); }
 
-std::vector<int> ListGraph::FindAllAdjacentIn(int vertex) const {
+std::vector<std::size_t> ListGraph::FindAllAdjacentIn(std::size_t vertex) const {
   return in_edges_[vertex];
 }
-std::vector<int> ListGraph::FindAllAdjacentOut(int vertex) const {
+std::vector<std::size_t> ListGraph::FindAllAdjacentOut(std::size_t vertex) const {
   return out_edges_[vertex];
 }
 
-void DFS_for_inverted(const ListGraph &graph, std::vector<int> &leave,
+void DFS_for_inverted(const ListGraph &graph, std::vector<std::size_t> &leave,
                       std::vector<bool> &used, int current, int &time) {
   used[current] = true;
-  for (int i : graph.FindAllAdjacentIn(current)) {
+  for (std::size_t i : graph.FindAllAdjacentIn(current)) {
     if (!used[i])
       DFS_for_inverted(graph, leave, used, i, time);
   }
-  leave[current] = time++;
+  ++time;
+  leave.emplace_back(current);
 }
 
 void DFS(const ListGraph &graph, std::vector<int> &components,
          std::vector<std::pair<bool, bool>> &in_out, int current,
          int current_component) {
   components[current] = current_component;
-  for (int i : graph.FindAllAdjacentOut(current)) {
+  for (std::size_t i : graph.FindAllAdjacentOut(current)) {
     if (components[i] == -1)
       DFS(graph, components, in_out, i, current_component);
     if (components[i] != current_component) {
@@ -58,19 +60,19 @@ void DFS(const ListGraph &graph, std::vector<int> &components,
 
 void Kosaraju(const ListGraph &graph,
               std::vector<std::pair<bool, bool>> &in_out) {
-  std::vector<int> leave(graph.VerticesCount());
+  std::vector<std::size_t> leave;
   std::vector<bool> used(graph.VerticesCount());
   int time = 0;
-  for (int i = 0; i < graph.VerticesCount(); ++i) {
+  for (std::size_t i = 0; i < graph.VerticesCount(); ++i) {
     if (!used[i])
       DFS_for_inverted(graph, leave, used, i, time);
   }
   std::vector<int> components(graph.VerticesCount(), -1);
-  int components_number = 0;
+  int components_number = 0;  
   for (int i = leave.size() - 1; i >= 0; --i) {
     if (components[leave[i]] == -1) {
       in_out.emplace_back(false, false);
-      DFS(graph, components, in_out, i, components_number++);
+      DFS(graph, components, in_out, leave[i], components_number++);
     }
   }
 }
@@ -84,6 +86,8 @@ std::size_t number_of_edges_to_be_added(const ListGraph &graph) {
   int outcoming_sum = 0;
   int isolated_sum = 0;
   for (auto i : in_out) {
+    if(i.first && i.second)
+      continue;
     if (i.first)
       ++incoming_sum;
     if (i.second)
@@ -100,10 +104,10 @@ int main() {
   int v = 0;
   int e = 0;
   std::cin >> v >> e;
-  int from = 0;
-  int to = 0;
+  std::size_t from = 0;
+  std::size_t to = 0;
   ListGraph graph(v);
-  for (int i = 0; i < e; ++i) {
+  for (std::size_t i = 0; i < e; ++i) {
     std::cin >> from >> to;
     graph.AddEdge(from - 1, to - 1);
   }
