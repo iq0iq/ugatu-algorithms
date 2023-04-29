5fca00399d6da369bc83839b74474768fcb8943b
Dinara L <dinaralatypova16@mail.ru>
2023-04-29 15:28:11 +0500

    task2: init solution

---
+++ term2/module_3/task2/task2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string min_string(const std::vector<uint32_t> &prefix) {
+  std::string result = "a";
+  for (uint32_t i = 1; i < prefix.size(); ++i) {
+    if (prefix[i] != 0) {
+      result.push_back(result[prefix[i] - 1]);
+    } else {
+      uint32_t k = i - 1;
+      std::vector<bool> allowed_letters(25, true);
+      while (prefix[k] != 0) {
+        allowed_letters[result[prefix[k]] - 'a'] = false;
+        k = prefix[k] - 1;
+        if (prefix[k] == k)
+          break;
+      }
+      for (uint32_t j = 1; j < 26; ++j) {
+        if (allowed_letters[j]) {
+          result.push_back(static_cast<char>('a' + j));
+          break;
+        }
+      }
+    }
+  }
+  return result;
+}
+
+int main() {
+  std::vector<uint32_t> prefix;
+  uint32_t x;
+  while (std::cin >> x)
+    prefix.emplace_back(x);
+  std::cout << min_string(prefix) << std::endl;
+  return 0;
+}
