d0cbe16e56b9e35a4d2e7bd2f39e816c608c1ab5
Dinara L <dinaralatypova16@mail.ru>
2023-05-04 23:08:22 +0500

    task2: change2

--- term2/module_3/task2/task2.cpp
+++ term2/module_3/task2/task2.cpp
@@ -3,22 +3,23 @@
 #include <vector>
 
 std::string min_string(const std::vector<uint32_t> &prefix) {
+  const uint8_t alphabet_size = 26;
   std::string result = "a";
   for (uint32_t i = 1; i < prefix.size(); ++i) {
     if (prefix[i] != 0) {
       result.push_back(result[prefix[i] - 1]);
     } else {
       uint32_t k = i - 1;
-      std::vector<bool> allowed_letters(25, true);
+      std::vector<bool> allowed_letters(alphabet_size - 1, true);
       while (prefix[k] != 0) {
         allowed_letters[result[prefix[k]] - 'a'] = false;
         k = prefix[k] - 1;
         if (prefix[k] == k)
           break;
       }
-      for (uint32_t j = 1; j < 26; ++j) {
+      for (char j = 1; j < alphabet_size; ++j) {
         if (allowed_letters[j]) {
-          result.push_back(static_cast<char>('a' + j));
+          result.push_back('a' + j);
           break;
         }
       }
